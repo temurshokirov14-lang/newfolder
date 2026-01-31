52d5fe0daff365b5b9b240fde438fd0c3c43b187
temurshokirov14-lang <temurshokirov14@gmail.com>
2026-01-31 17:21:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let person = {};
+
+let name = prompt("Ismingizni kiriting");
+
+let ID = prompt(" Mars IT ID ni kiriting");
+
+let parol = prompt(" Mars IT paroli ni kiriting");
+
+
+
+person.ism = name;
+person.ID = ID;
+person.parol = parol;
+
+console.log(person);
